7b5b3ee67e695b35554ed6aa8aea0ba34d48a87a
Brent <schneeman@gmail.com>
2023-06-01 09:03:19 -0500

    refactoring

--- cover-chain.py
+++ cover-chain.py
@@ -1,15 +1,13 @@
-from langchain.chat_models import ChatOpenAI
+from dotenv import dotenv_values, load_dotenv
+from langchain.callbacks import get_openai_callback
 from langchain.chains import LLMChain
 from langchain.chains.summarize import load_summarize_chain
-from langchain.callbacks import get_openai_callback
-from langchain.prompts import PromptTemplate
-from langchain.document_loaders import OnlinePDFLoader
-from langchain.document_loaders import PyPDFLoader
+from langchain.chat_models import ChatOpenAI
 from langchain.docstore.document import Document
+from langchain.document_loaders import (OnlinePDFLoader, PyPDFLoader,
+                                        UnstructuredFileLoader)
+from langchain.prompts import PromptTemplate
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.document_loaders import UnstructuredFileLoader
-
-from dotenv import load_dotenv, dotenv_values
 
 # This will (likely) generate a cover letter based on a Resume
 # and a job specification. The resume is a PDF and the spec is
@@ -52,8 +50,7 @@ def main():
         openai_api_key=config["OPENAI_API_KEY"],
     )
 
-    # this can be "map_reduce", "refine", or "stuff" - not sure which is 'best'
-    chain_summarize = load_summarize_chain(llm_summarize, "refine")
+
 
     #loader = OnlinePDFLoader("https://tworavens.ai/schneeman-brent-resume.pdf")
     loader = PyPDFLoader("./schneeman-brent-resume.pdf")
@@ -76,9 +73,21 @@ def main():
     # Summarize the spec. This is mostly to reduce the token size of the 
     # spec to something that can fit into the "generate the cover letter"
     # prompt. If the spec is short enough, it could be used directly.
+
+    # this can be "map_reduce", "refine", or "stuff" - not sure which is 'best'
+    chain_summarize = load_summarize_chain(llm_summarize, "refine")
+
     summarized_spec = chain_summarize.run(docs)
     print(summarized_spec)
 
+    cover_txt = generate_cover(llm_generate, the_resume, summarized_spec)
+    print(cover_txt)
+
+    # dump it out into a file
+    with open('cover-letter.txt', 'w') as f:
+        f.writelines(cover_txt)
+
+def generate_cover(llm_generate, the_resume, summarized_spec):
     prompt_template = """Use the jobspec below to write a cover letter based on
     the resume, and do not assert experience that is not in the resume. 
     Be professional, and emphasize leadership experience mentioned in the resume.
@@ -97,11 +106,7 @@ def main():
     chain = LLMChain(llm=llm_generate, prompt=PROMPT)
     cover_letter = chain.apply([{"jobspec": summarized_spec, "resume": the_resume[0].page_content}])
     cover_txt = cover_letter[0]["text"]
-    print(cover_txt)
-
-    # dump it out into a file
-    with open('cover-letter.txt', 'w') as f:
-        f.writelines(cover_txt)
+    return cover_txt
 
 
 if __name__ == "__main__":
