15beee53ba4595929f8b1910e049d48e77f0acff
Brent <schneeman@gmail.com>
2023-10-23 15:18:19 -0500

    cleaning up comments

--- cover-chain.py
+++ cover-chain.py
@@ -2,8 +2,7 @@ from langchain.callbacks import get_openai_callback
 from langchain.chains import LLMChain
 from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
-from langchain.document_loaders import (PyPDFLoader,
-                                        UnstructuredFileLoader)
+from langchain.document_loaders import (UnstructuredFileLoader)
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import CharacterTextSplitter
 
@@ -53,46 +52,11 @@ def generate_cover_letter(resume, job_spec):
 
 def main():
 
-
-    #loader = OnlinePDFLoader("https://tworavens.ai/schneeman-brent-resume.pdf")
-    #loader = PyPDFLoader("./schneeman-brent-resume.pdf")
-
     the_resume = extract_text_from_pdf("./schneeman-brent-resume.pdf" )
 
     text_loader = UnstructuredFileLoader("./job-spec.txt")
 
     the_spec = text_loader.load()
-
-    # # splitting the spec into chunks to summarize.
-    # # using a 1000 token chunk size (with the default overlap)
-    # # of 200 tokens) to attempt to extract specifics while 
-    # # maintaining overall context. This is 100% a guess
-    # text_splitter = CharacterTextSplitter(chunk_size=1500) 
-    # the_spec_chunks = text_splitter.split_text(the_spec[0].page_content)
-
-    # docs = [Document(page_content=t) for t in the_spec_chunks]
-
-
-    # # two llms, one for the summary (that specs max_tokens) and one
-    # # for the generation. I found that this arrangement works best
-    # # to not overrun the buffer size
-
-    # factory = LLMFactory()
-
-    # llm_summarize = factory.create_summarizer()
-    # llm_generate = factory.create_generator()
-
-    # # Summarize the spec. This is mostly to reduce the token size of the 
-    # # spec to something that can fit into the "generate the cover letter"
-    # # prompt. If the spec is short enough, it could be used directly.
-
-    # # this can be "map_reduce", "refine", or "stuff" - not sure which is 'best'
-    # chain_summarize = load_summarize_chain(llm_summarize, "refine")
-
-    # summarized_spec = chain_summarize.run(docs)
-    # print(summarized_spec)
-
-    # cover_txt = generate_cover(llm_generate, the_resume, summarized_spec)
 
     cover_txt = generate_cover_letter(the_resume, the_spec)
 
