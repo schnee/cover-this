6e8bce21c9714562072489d22579e780043d287b
Brent <schneeman@gmail.com>
2023-10-24 11:09:00 -0500

    wrapping text

--- cover_this_app.py
+++ cover_this_app.py
@@ -4,15 +4,15 @@ from io import StringIO
 
 st.title("Cover Letter Generator")
 
-st.text("This app will generate a cover letter based on a PDF resume and a job spec in a text file. Simply upload both press the 'Generate' button.")
-st.link_button("Buy me a coffee", "https://www.buymeacoffee.com/mahkr")
+st.markdown("This app will generate a cover letter based on a PDF resume and a job spec in a text file. Simply upload both and press the 'Generate' button.")
+st.link_button(":orange[Buy me a coffee]", "https://www.buymeacoffee.com/mahkr",)
+#st.markdown("[![Buy me a coffee](https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png)](https://www.buymeacoffee.com/mahkr)")
 st.link_button("Email me with questions", "mailto:schneeman@gmail.com")
 st.header("Upload Resume")
 resume = st.file_uploader("Choose a resume", type=["pdf"])
 
 st.header("Upload Job Spec")  
 job_spec = st.file_uploader("Choose a job spec", type=["txt"])
-
 
 if st.button("Generate Cover Letter"):
     if resume and job_spec:
